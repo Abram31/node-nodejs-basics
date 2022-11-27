156113cc63187f5a4f8b090633ecfa45d8edd6ae
Abram31 <a447623993@gmail.com>
2022-11-27 18:39:05 +0300

    feat: created function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile, access } from 'fs';
+import { join, resolve } from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const pathFile = resolve(join('src', 'fs', 'files', 'fresh.txt'));
+    access(pathFile, (err) => {
+        if (!err) return console.error("FS operation failed")
+        else {
+            writeFile(pathFile, "I am fresh and young", (err) => {
+                if (err) throw err;
+                console.log('File created');
+            })
+        }
+
+    })
 };
 
 await create();
