64e0959b5c0dcf80d5e53271d6deaebbacb66a40
Abram31 <a447623993@gmail.com>
2022-12-03 20:01:25 +0300

    feat: implements cli:env and cli:args

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const result = Object.entries(process.env).reduce((acc, item) => {
+        if (item[0].startsWith('RSS_')) {
+            acc.push(`${item[0]}=${item[1]}`)
+        }
+        return acc
+    },[]).join('; ');
+
+    console.log(result);
 };
 
 parseEnv();
