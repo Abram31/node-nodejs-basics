4657c6a5d0f11894459e847b3555046d8876e15f
Abram31 <a447623993@gmail.com>
2022-12-04 00:39:25 +0300

    feat: complete hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'node:crypto'
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const content = await readFile(join(__dirname, 'files/fileToCalculateHashFor.txt'));
+        const hash = createHash('sha256').update(content).digest('hex');
+        console.log(hash);
+    } catch (err) {
+        throw err
+    }
 };
 
-await calculateHash();
+calculateHash();
