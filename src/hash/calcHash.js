645be24d426f7b8c6b97a89cd4e497ba9696fadf
Abram31 <a447623993@gmail.com>
2022-12-04 19:27:43 +0300

    feat: completed streams

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,7 +1,7 @@
 import { createHash } from 'node:crypto'
-import { dirname, join } from 'path';
 import { readFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,12 @@
+import { Transform } from 'stream';
+import { stdin, stdout } from 'process';
+import { EOL } from 'os';
+
 const transform = async () => {
-    // Write your code here 
-};
+    const result = new Transform({transform(chunk, enc, callBack) {
+        callBack(null, chunk.toString().replace(EOL,'').split('').reverse().join(''))
+    }});
+    stdin.pipe(result).pipe(stdout)
+  };
 
 await transform();
