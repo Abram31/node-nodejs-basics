c8e7e2c87b49485b35896da5ab76f191512adeb6
Abram31 <a447623993@gmail.com>
2022-12-06 00:33:50 +0300

    feat: finished commit

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,7 +1,7 @@
 import { parentPort, workerData } from "worker_threads";
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-export const sendResult = () => {
+const sendResult = () => {
     parentPort.postMessage(nthFibonacci(workerData))
 };
 
